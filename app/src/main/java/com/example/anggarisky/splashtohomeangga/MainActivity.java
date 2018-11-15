1a2e25a4c3c1fb6a7ab760216fe776b3f3d7cc94
Angga Risky <AnggaRisky@Anggas-MacBook-Pro-Retina.local>
2018-11-15 10:41:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/anggarisky/splashtohomeangga/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.anggarisky.splashtohomeangga;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView bgapp, clover;
+    LinearLayout textsplash, texthome, menus;
+    Animation frombottom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        frombottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
+
+
+        bgapp = (ImageView) findViewById(R.id.bgapp);
+        clover = (ImageView) findViewById(R.id.clover);
+        textsplash = (LinearLayout) findViewById(R.id.textsplash);
+        texthome = (LinearLayout) findViewById(R.id.texthome);
+        menus = (LinearLayout) findViewById(R.id.menus);
+
+        bgapp.animate().translationY(-1900).setDuration(800).setStartDelay(300);
+        clover.animate().alpha(0).setDuration(800).setStartDelay(600);
+        textsplash.animate().translationY(140).alpha(0).setDuration(800).setStartDelay(300);
+
+        texthome.startAnimation(frombottom);
+        menus.startAnimation(frombottom);
+
+
+    }
+}
